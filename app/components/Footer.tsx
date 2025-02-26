1b8450fd0d513bd74e7de3f2409367ba9e3e0792
anas <ktmanas2020@gmail.com>
2025-02-26 21:48:17 +0530

    sama-alkhaleeg

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
 function Footer() {
-  return (
-    <div>
-    <footer className="bg-gray-800 text-white py-4">
-        <div className="container mx-auto text-center">
-            <p>&copy; {new Date().getFullYear()} Emenulab.All rights reserved.</p>
+    return (
+        <div>
+            <footer className="bg-gray-800 text-white py-4">
+                <div className="container mx-auto text-center">
+                    <p>&copy; {new Date().getFullYear()} emenulab.All rights reserved.</p>
+                </div>
+            </footer>
         </div>
-    </footer>
-    </div>
-  )
+    )
 }
 
 export default Footer
